c4f4d34ea764cb30ffb8575670e30f7a23413b6b
shelemetev <43704293+shelemetev@users.noreply.github.com>
2021-01-28 14:45:24 +0300

    Add files via upload

---
+++ untitled1/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int yearOfBirthday = 2002;
+	int monthOfBirthday = 11;
+	int dayOfBirthday = 23;
+
+    }
+}
